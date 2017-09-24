2affc4e125ae4c659e3291b25ff11623d684509c
Simon Liu <michaelblob99@gmail.com>
2017-09-24 04:14:38 -0400

    ML: Rewrite to 2.0

--- MovieList.py
+++ MovieList.py
@@ -1,61 +1,104 @@
 import os
-import xlrd
-import xlwt
+import sys
+import xlsxwriter
 
-class movie(object):
+class film(object):
     name = ''
     year = 0
 
-def makeNewMovie(dirName):
+class show(object):
+    name = ''
+    seasons = []
+
+def makeNewFilm(dirName):
     dNs = dirName.split()
     dNlen = dNs.__len__()
-    m = movie()
-    m.year = int(dNs.pop()[1:5])
-    m.name = ' '.join(dNs)
-    return m
+    f = film()
+    f.year = int(dNs.pop()[1:5])
+    f.name = ' '.join(dNs)
+    return f
 
-def makeMovie(n,y):
-    m = movie()
-    m.name = n
-    m.year = int(y)
-    return m
+def makeNewShow(dirName,seasons):
+    s = show()
+    s.name = dirName
+    seasonList = []
+    s.seasons = seasons
+    return s
 
-def writeXLS(mL):
-    book = xlwt.Workbook()
-    sh = book.add_sheet('Movies')
+def writeXLSX(fL,sL):
+    book = xlsxwriter.Workbook('Entertainment.xlsx')
+    font = book.add_format({'font_name':'Courier','font_size':12})
+    center = book.add_format({'font_name':'Courier','font_size':12,'align':'center'})
+    
+    films = book.add_worksheet('Films')
     maxLen = 0
-    for i,m in enumerate(mL):
-        sh.write(i,0,m.name)
-        sh.write(i,1,m.year)
-        maxLen = len(m.name) if len(m.name) > maxLen else maxLen
-    #sh.set_column(1, 1, maxLen + 5)
-    book.save('MoviesTemp.xls')
+    for i,f in enumerate(fL):
+        films.write(i,0,f.name,font)
+        films.write(i,1,f.year,center)
+        maxLen = len(f.name) if len(f.name) > maxLen else maxLen
+    films.set_column(0,0,maxLen+12)
 
-def readXLS(fN):
-    book = xlrd.open_workbook(fN)
-    sh = book.sheet_by_index(0)
-    mL = []
-    for j in range(0,sh.nrows):
-        mL.append(makeMovie(sh.cell(j,0).value,sh.cell(j,1).value))
-    return mL
-        
-def main():
-    os.chdir('G:\\Movies')
-    curDir = os.getcwd()
-    print curDir
-    mListInit = readXLS('Movies.xls')
+    shows = book.add_worksheet('Shows')
+    maxLen = 0
+    maxSeason = 0
+    for j,s in enumerate(sL):
+        shows.write(j,0,s.name,font)
+        maxLen = len(s.name) if len(s.name) > maxLen else maxLen
+        for k,n in enumerate(s.seasons):
+            shows.write(j,k+1,n,center)
+            maxSeason = len(s.seasons) if len(s.seasons) > maxSeason else maxSeason
+    shows.set_column(0,0,maxLen+10)
+    shows.set_column(1,maxSeason,20)
+
+    book.close()
+
+def dirFix(mD):
+    curDir = os.path.join(mD,'Films')
+    os.chdir(curDir)
     dirList = [d for d in os.listdir(curDir) if os.path.isdir(os.path.join(curDir, d))]
-    mList = []
-    print ' '
-    for m in dirList:
-        mX = makeNewMovie(m)
-        mU = True
-        for mI in mListInit:
-            mU = False if mX.name == mI.name else mU
-        mList.append(mX) if mU == True else mU
-    mList.extend(mListInit)
-    os.remove('Movies.xls')
-    writeXLS(mList)
-    os.rename('MoviesTemp.xls','Movies.xls')
+    for i in dirList:
+        dirSplit = i.split()
+        checked = False
+        while checked != True:
+            if dirSplit[-1][0] == '[' and dirSplit[-1][-1] == ']':
+                print(dirSplit[-1]),
+                dirSplit.pop()
+                print(' '.join(dirSplit))
+            else:
+                checked = True
+        try:
+            os.rename(i,' '.join(dirSplit))
+        except:
+            print('Could not rename:', i)
+    os.chdir(mD)
+    input("Press Enter to continue...")
+        
+def films(mD):
+    curDir = os.path.join(mD,'Films')
+    dirList = [d for d in os.listdir(curDir) if os.path.isdir(os.path.join(curDir, d))]
+    fList = []
+    for f in dirList:
+        fList.append(makeNewFilm(f))
+    return fList
+
+def shows(mD):
+    curDir = os.path.join(mD,'Shows')
+    dirList = [d for d in os.listdir(curDir) if os.path.isdir(os.path.join(curDir, d))]
+    sList = []
+    for s in dirList:
+        curSub = os.path.join(curDir,s)
+        subList = [u for u in os.listdir(curSub) if os.path.isdir(os.path.join(curSub, u))]
+        sList.append(makeNewShow(s,subList))
+    return sList
+
+def main():
+    mainDir = os.getcwd()
+    print(mainDir)
+    dirFix(mainDir)
+    fL = films(mainDir)
+    sL = shows(mainDir)
+    writeXLSX(fL,sL)
+    print(len(fL),'movies,',len(sL),'shows')
+    input("Press Enter to exit...")
 
 main()
