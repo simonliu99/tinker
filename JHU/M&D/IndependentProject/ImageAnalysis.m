c37bb620b0f02679150d4d805fb829194e318f1b
Simon Liu <michaelblob99@gmail.com>
2017-12-07 22:06:12 -0500

    OPTICS: True final changes to imageAnalysis and plot creation for poster

--- JHU/M&D/IndependentProject/ImageAnalysis.m
+++ JHU/M&D/IndependentProject/ImageAnalysis.m
@@ -50,7 +50,7 @@ flFlat = zeros(1,64);
 for i = 1:8
     for j = 1:8
         theta2(i,j) = abs(ang(i,j)-90+theta1(j)-90);
-        flFlat((j-1)*8+i) = abs((i-1)*0.5*tan(deg2rad(theta2(i,j)+90-theta1(j))))*0.116;
+        flFlat((j-1)*8+i) = abs((i-1)*0.5*tan(deg2rad(theta2(i,j)+90-theta1(j))))*0.0116;
     end
 end
 
@@ -66,23 +66,74 @@ for k = 1:8
 end
 
 % manual calculation
-flFlat(1,6) = 18;
-flFlat(1,7) = 20;
+flFlat(1,6) = 2.056;
+flFlat(1,7) = 2.108;
+
+% normalization of focal lengths to match model
+flFlatNorm = zeros(1,64);
+for m = 1:64
+    flFlatNorm(m) = flFlat(m)*6.130435155/max(flFlat);
+end
 
 % mesh plot
 figure;
 [xq,yq] = meshgrid(0.16:0.05:6.25,1.35:0.0003:1.41);
-vq = griddata(dC,flip(riG),flFlat,xq,yq);
+vq = griddata(dC,flip(riG),flFlatNorm,xq,yq);
 mesh(xq,yq,vq)
 hold on
-plot3((dC),flip(riG),flFlat,'o')
+plot3((dC),flip(riG),flFlatNorm,'o');
 xlim([0.16 6.25])
-% ylim([1.383 1.4025])
-zlim([0 22])
+zlim([0 6.14])
 xlabel('Curvature (cm^-^1)')
 ylabel('Refractive Index')
 zlabel('Focal Length (cm)')
 title('Experimental Focal Length versus Degree of Curvature and Refraction Index')
+
+% model plot
+figure;
+[xq,yq] = meshgrid(0.16:0.02:6.25,1.35:0.001:1.41);
+vq = griddata(dC,riG,model(),xq,yq);
+mesh(xq,yq,vq)
+hold on
+plot3((dC),riG,model(),'o');
+xlim([0.16 6.25])
+zlim([0 6.14])
+xlabel('Curvature (cm^-^1)')
+ylabel('Refractive Index')
+zlabel('Focal Length (cm)')
+title('Maths Model of Focal Length versus Degree of Curvature and Refraction Index')
+
+% residual plot preparation
+mod = model();
+flFlatNormSwap = zeros(1,64);
+for n = 1:8
+    for o = 1:8
+        flFlatNormSwap((n-1)*8+o) = flFlatNorm((n-1)*8+(8-o+1));
+    end
+end
+
+% ssim calculation between experimental and model data
+[ssimval, ssimmap] = ssim(flFlatNormSwap,mod);
+ssimval
+figure, imshow(ssimmap,[]);
+res = mod - flFlatNormSwap;
+figure, scatter(1:64, res);
+xlim([0,64])
+xlabel('Trial')
+ylabel('Residual (cm)')
+title('Discrepancy Between Model and Experimental Data')
+
+% residual plot
+figure;
+[xq,yq] = meshgrid(0.16:0.001:6.25,1.35:0.0001:1.41);
+vq = griddata(dC,riG,res,xq,yq);
+mesh(xq,yq,vq)
+hold on
+xlabel('Curvature (cm^-^1)')
+ylabel('Refractive Index')
+zlabel('Residual (cm)')
+title('Residual Heat Map')
+
 end
 
 % analyze image to find bright spots and return vectors and angle
@@ -133,4 +184,39 @@ end
 function [r,c] = toR(x,y)
 r = sum(x)/size(x,1);
 c = sum(y)/size(y,1);
+end
+
+function [xG] = model()
+ri = [1.375177282, 1.379726782, 1.384001458, 1.388024636, 1.391817202, 1.395397886, 1.398783522, 1.401989263]; % constant refractive index values
+
+% data from Google Sheets
+x = [6.130435155	3.058554061	1.52927703	1.01951802	0.7646385152	0.6117108122	0.5097590101	0.4369362943
+6.056986527	3.021909583	1.510954791	1.007303194	0.7554773956	0.6043819165	0.5036515971	0.4317013689
+5.98956059	2.98269903	1.494134952	0.9960899677	0.7470674758	0.5976539806	0.4980449839	0.4268957004
+5.927458682	2.957286451	1.478643226	0.9857621504	0.7393216128	0.5914572902	0.4928810752	0.422469493
+5.870084292	2.92866162	1.46433081	0.9762205399	0.7321654049	0.5857323239	0.4881102699	0.4183802313
+5.816925388	2.902139949	1.451069975	0.9673799831	0.7255349873	0.5804279898	0.4836899915	0.4145914212
+5.767540221	2.877501045	1.438750523	0.959167015	0.7193752613	0.575500209	0.4795835075	0.4110715778
+5.721545844	2.854553851	1.427276925	0.9515179502	0.7136384627	0.5709107701	0.4757589751	0.4077934072];
+
+% create one-dimensional matrix
+xG = zeros(1,64);
+for i = 1:8
+    for j = 1:8
+        xG((j-1)*8+i) = x(i,j);
+    end
+end
+% xG
+
+% create dC and riG matrices
+thet = [2.3	1.1475	0.57375	0.3825	0.286875	0.2295	0.19125	0.1639285714];
+dC = zeros(1,64);
+riG = zeros(1,64);
+
+for k = 1:8
+    for l = 1:8
+        riG((k-1)*8+l) = ri(l);
+        dC((k-1)*8+l) = 1/thet(k);
+    end
+end
 end
